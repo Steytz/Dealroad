17b458713e30d2df82604ace02d3a51c6a7d15cd
Steytz <vluciano@codevise.de>
2022-10-03 21:12:48 +0200

    Add starting color palette
    - The initial color palette hence figma design was added

---
+++ theme/palette.ts
@@ -0,0 +1,15 @@
+type TPalette = {
+  orange: string
+  white: string
+  blue: string
+  black: string
+  red: string
+}
+
+export const palette: TPalette = {
+  orange: "#fb8b24",
+  white: "#fbfef9",
+  blue: "#0197f6",
+  black: "#02182b",
+  red: "#d7263d",
+}
